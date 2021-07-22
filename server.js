e63a7a40abb63234fd9e311dd0cd15a043e963ce
Nicholas-Mooney <60123281+Nicholas-Mooney@users.noreply.github.com>
2021-07-22 15:59:51 -0700

    added routes

--- server.js
+++ server.js
@@ -1,37 +1,12 @@
-const express = require("express");
-const app = express();
-let fs = require('fs');
-const port = process.env.PORT || 8000;
+let http = require('http');
+let router = require('./route');
 
-app.get(["/index.html", "/:name"], (request, response) => {
-
+let handleRequest = (request, response) => {
   response.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  fs.readFile('./index.html', null, function (error, data) {
-    if (error) {
-      response.writeHead(404);
-      response.write('Whoops! File not found!');
-    } else {
-      response.write(data);
-    }
-    response.end();
-  });
-});
-app.get(["/signUp.html"], (request, response) => {
 
-  response.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-  fs.readFile('./signUp.html', null, function (error, data) {
-    if (error) {
-      response.writeHead(404);
-      response.write('Whoops! File not found!');
-    } else {
-      response.write(data);
-    }
-    response.end();
-  });
-});
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`))
+};
+
+http.createServer(router.handleRequest).listen(8000);
 
